4a5961c2e07f04609951284e2105d82513be1562
GreedNeSS <ruslan199126@gmail.com>
2022-02-06 15:06:45 +0300

    Добавьте файлы проекта.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Extensions;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12345678;
+            number.DisplayDefiningAssembly();
+
+            System.Data.DataSet dataSet = new();
+            dataSet.DisplayDefiningAssembly();
+
+            System.Data.SqlDbType sqlDbType = new();
+            sqlDbType.DisplayDefiningAssembly();
+
+            Console.WriteLine($"number: {number}");
+            Console.WriteLine($"Reversed digits of number: " +
+                number.ReverseDigits());
+        }
+    }
+}
