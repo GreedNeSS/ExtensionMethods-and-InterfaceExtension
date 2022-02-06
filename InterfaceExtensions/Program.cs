f6b0c094f26fa0eb28b3466afbc3435fd229159b
GreedNeSS <ruslan199126@gmail.com>
2022-02-06 15:58:59 +0300

    Use interface extension

---
+++ InterfaceExtensions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 5, 6, 7 };
+            numbers.PrintDataAndBeep();
+
+            Console.WriteLine();
+
+            List<string> strings = new()
+            {
+                "Hello",
+                "World",
+                "Bye"
+            };
+            strings.PrintDataAndBeep();
+        }
+    }
+}
